51a4bbbed43b9c759d9e99ff4a34bef10dea7602
SarangKumar <sarangkumar1578@gmail.com>
2022-11-08 21:47:38 +0530

    Adding final changes to the styling branch

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,7 +1,7 @@
-const hours = 24;
-const start = 1667711162738;
+const hours = 2;
+const start = 1667818704380;
 
 const startTime = (start/1000).toFixed(0);
-const finalTime = (start/1000+hours*3600).toFixed(0);
+const finalTime = (start/1000+(hours*3600)).toFixed(0);
 
 export {finalTime, startTime, hours};
